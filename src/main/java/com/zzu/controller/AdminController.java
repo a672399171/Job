bf451303072c40088cb52074eda2956541600c34
zhanglei53 <zhanglei53@jd.com>
2016-08-09 08:47:21 +0800

    c

---
+++ src/main/java/com/zzu/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.zzu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhanglei53 on 2016/8/6.
+ */
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+}
--- web/admin/dashboard/ctrl.js
+++ web/admin/dashboard/ctrl.js
@@ -1,7 +1,7 @@
 app.controller('DashboardController', function ($scope, $state, $http) {
     //查询
     $scope.query = function () {
-        $http.get($scope.app.host + "/user/admin/dashboard.do")
+        $http.get("/admin/dashboard")
             .then(function(response) {
                 $scope.poorCount = response.data.poorCount;
                 $scope.companyCount = response.data.companyCount;
